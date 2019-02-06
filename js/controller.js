293802d2bb47a37a975220b8f1789027a3fa24c0
thezeeck <estigma.e@gmail.com>
2019-02-06 00:15:08 -0600

    Files
    Files

---
+++ js/controller.js
@@ -0,0 +1,37 @@
+$(function(data){
+  $.when($("#norte").load("templates/norte.html")).then(function(){});
+  $.when($("#sur").load("templates/sur.html")).then(function(){});
+});
+
+function carousel() {
+	 var i,x = $(".slide"),y=$(".carousel_indicator");
+   for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      y[i].classList.remove("current-slide");
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    y[slideIndex-1].classList.add("current-slide");
+    setTimeout(carousel, 8000);
+}
+function cambia(cual){
+  var i,x = $(".slide"),y=$(".carousel_indicator");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+     y[i].classList.remove("current-slide");
+   }
+   slideIndex=cual;
+   x[slideIndex].style.display = "block";
+   y[slideIndex].classList.add("current-slide");
+}
+function caruselBaner(){
+  var i,x = $(".itemDescripcionBeneficio");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+   }
+   beneficioIndex++;
+   if (beneficioIndex > x.length) {beneficioIndex = 1}
+   x[beneficioIndex-1].style.display = "flex";
+   setTimeout(caruselBaner, 8000);
+}
