1fd21961af4c0d06605d3addaa8dddd0faf91145
AntonioCampa <antonio_cs@outlook.com>
2019-02-12 01:00:16 -0600

    Chages in Terms
    Cambios a la seccion terminos

--- js/controller.js
+++ js/controller.js
@@ -57,3 +57,17 @@ function caruselBaner(){
    x[beneficioIndex-1].style.display = "flex";
    setTimeout(caruselBaner, 8000);
 }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
