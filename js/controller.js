88a6a3e00c35359c8e6bf1601017247301f7e91e
thezeeck <estigma.e@gmail.com>
2019-02-12 01:20:48 -0600

    terminos

--- js/controller.js
+++ js/controller.js
@@ -57,17 +57,24 @@ function caruselBaner(){
    x[beneficioIndex-1].style.display = "flex";
    setTimeout(caruselBaner, 8000);
 }
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
+function terms() {
+  $(".accordion").on("click", function(e) {
+    $(e.currentTarget).toggleClass("active").next().slideToggle();
   });
+
+
+
+  // var acc = document.getElementsByClassName("accordion");
+  //
+  // for (var i = 0; i < acc.length; i++) {
+  //   acc[i].addEventListener("click", function() {
+  //     this.classList.toggle("active");
+  //     var panel = this.nextElementSibling;
+  //     if (panel.style.maxHeight){
+  //       panel.style.maxHeight = null;
+  //     } else {
+  //       panel.style.maxHeight = panel.scrollHeight + "px";
+  //     }
+  //   });
+  // }
 }
