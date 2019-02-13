d923566ea8fc2dc3110ed8eefe3c0d4aca663e9d
GamaHerrera <47377998+GamaHerrera@users.noreply.github.com>
2019-02-12 23:31:57 -0600

    Update _politicas.scss

--- js/controller.js
+++ js/controller.js
@@ -57,3 +57,24 @@ function caruselBaner(){
    x[beneficioIndex-1].style.display = "flex";
    setTimeout(caruselBaner, 8000);
 }
+function terms() {
+  $(".accordion").on("click", function(e) {
+    $(e.currentTarget).toggleClass("active").next().slideToggle();
+  });
+
+
+
+  // var acc = document.getElementsByClassName("accordion");
+  //
+  // for (var i = 0; i < acc.length; i++) {
+  //   acc[i].addEventListener("click", function() {
+  //     this.classList.toggle("active");
+  //     var panel = this.nextElementSibling;
+  //     if (panel.style.maxHeight){
+  //       panel.style.maxHeight = null;
+  //     } else {
+  //       panel.style.maxHeight = panel.scrollHeight + "px";
+  //     }
+  //   });
+  // }
+}
